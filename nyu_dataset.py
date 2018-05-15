ba7ec5d78c0e21b538da0163d58504a34336b0b4
Aliaksandra Shysheya <afternonsister@gmail.com>
2018-05-15 19:22:57 +0300

    changes in Dataset class

--- nyu_dataset.py
+++ nyu_dataset.py
@@ -1,17 +1,14 @@
 import scipy.misc
 import numpy as np
-import h5py
 import torchvision.transforms as transforms
 from PIL import Image
 import torch
 from torch.utils.data import Dataset
-
-VAL_SIZE = 249
-TRAIN_SIZE = 1200
+import glob
 
 
 class SegmentationTransform(object):
-    def __init__(self, train_trainsforms=True, num_classes=894):
+    def __init__(self, train_trainsforms=True, num_classes=40):
         self.train_trainsforms = train_trainsforms
         self.to_tensor = transforms.ToTensor()
         if train_trainsforms:
@@ -20,10 +17,7 @@ class SegmentationTransform(object):
             self.resize = transforms.Resize(size=(256, 256))
         self.num_classes = num_classes
 
-    def __call__(self, input_image, input_segmentation):
-
-        image = transforms.ToPILImage()(np.transpose(input_image))
-        segmentation = np.transpose(input_segmentation)
+    def __call__(self, image, segmentation):
 
         if self.train_trainsforms:
             image = self.resize(image)
@@ -53,30 +47,21 @@ class SegmentationTransform(object):
 
 
 class SegmentationDataset(Dataset):
-    def __init__(self, path_to_file='nyu_depth_v2_labeled.mat', transforms=None, val=False):
+    def __init__(self, path_to_datafolder='./datasets/nyu/train/', transforms=None, val=False):
         if transforms:
             self.transforms = transforms
         else:
             self.transforms = None
 
-        self.val = val
-
-        # leave 249 for validation, other samples for training
-        if val:
-            self.size = VAL_SIZE
-        else:
-            self.size = TRAIN_SIZE
-
-        self.data = h5py.File(path_to_file, 'r')
+        self.image_filenames = sorted(glob.glob('{}image/*.png'.format(path_to_datafolder)))
+        self.segm_filenames = sorted(glob.glob('{}segm/*.png'.format(path_to_datafolder)))
 
     def __len__(self):
-        return self.size
+        return len(self.image_filenames)
 
     def __getitem__(self, idx):
-        if self.val:
-            idx += TRAIN_SIZE
+        image = scipy.misc.imread(self.image_filenames[idx])
+        segmentation = scipy.misc.imread(self.segm_filenames[idx])
         if self.transforms:
-            im, mask = self.transforms(self.data['images'][idx], self.data['labels'][idx])
-            return im, mask
-        else:
-            return self.data['images'][idx], self.data['labels'][idx]
+            image, segmentation = self.transforms(image, segmentation)
+        return image, segmentation
